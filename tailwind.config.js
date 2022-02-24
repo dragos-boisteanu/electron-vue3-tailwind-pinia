bf796b22c8311585651371bf3daac2e10e43d79f
Dragos <dragos360@yahoo.com>
2022-02-24 23:57:32 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+const colors = require('tailwindcss/colors')
+
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+		colors: {
+			...colors
+		}
+  },
+  plugins: [],
+}
