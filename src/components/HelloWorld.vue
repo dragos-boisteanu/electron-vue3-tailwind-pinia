a0f60e8212668fe4288c17c10c5ad38e821abfb5
Dragos <dragos360@yahoo.com>
2022-02-27 20:56:16 +0200

    added pinia with demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,9 +9,7 @@
 	</p>
 
 	<p>
-		<a href="https://vitejs.dev/guide/features.html" target="_blank">
-			Vite Documentation
-		</a>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Documentation </a>
 		|
 		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
 	</p>
@@ -21,16 +19,82 @@
 		Edit
 		<code>components/HelloWorld.vue</code> to test hot module replacement.
 	</p>
+
+	<div class="flex flex-col align-top justify-center p-5 gap-y-5">
+		<div>
+			<span class="font-semibold"
+				>Counter: <span>{{ store.count }}</span></span
+			>
+		</div>
+		<div>
+			<span class="font-semibold">My Name: </span> <span> {{ myName }}</span>
+		</div>
+		<div v-if="upperCaseMyName.length > 0">
+			<span class="font-semibold">My Name Uppercase: </span>
+			<span> {{ upperCaseMyName }}</span>
+		</div>
+		<div class="flex align-middle justify-center gap-x-5">
+			<button
+				class="px-4 py-1 rounded-sm bg-teal-800 hover:bg-teal-500 text-white ext-center"
+				@click="store.increment()"
+			>
+				+
+			</button>
+			<button
+				class="px-4 py-1 rounded-sm bg-red-500 hover:bg-red-400 text-white ext-center"
+				@click="store.resetCount()"
+			>
+				Reset
+			</button>
+			<button
+				class="px-4 py-1 rounded-sm bg-teal-800 hover:bg-teal-500 text-white ext-center"
+				@click="store.decrement()"
+			>
+				-
+			</button>
+		</div>
+		<div class="flex align-middle justify-center p-4">
+			<input
+				class="p-2 rounded-sm border border-slate-700 focus:border-slate-500 text-xs text-gray-600 outline-none"
+				v-model="myName"
+			/>
+		</div>
+		<div class="flex align-middle justify-center p-4 gap-x-4">
+			<input
+				class="p-2 rounded-sm border border-slate-700 focus:border-slate-500 text-xs text-gray-600 outline-none"
+				v-model.number="newCounterValue"
+			/>
+			<button class="px-4 py-1 rounded-sm bg-teal-800 text-white ext-center" @click="store.setCount(newCounterValue)">
+				Save
+			</button>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { counterStore } from '../stores/counter'
 
 defineProps({
 	msg: String,
 })
 
-const count = ref(0)
+const store = counterStore()
+
+function incrementCounter() {
+	store.increment()
+}
+
+function decrementCounter() {
+	store.decrement()
+}
+
+const myName = ref('')
+const newCounterValue = ref(0)
+
+const upperCaseMyName = computed(() => {
+	return myName.value.toUpperCase()
+})
 </script>
 
 <style scoped>
---
+++ src/stores/counter.js
@@ -0,0 +1,32 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const counterStore = defineStore('counter', () => {
+	const count = ref(0)
+
+	const doubleCount = computed(() => {
+		return count.value * 2
+	})
+
+	function increment() {
+		count.value++
+	}
+
+	function decrement() {
+		if (count.value > 0) {
+			count.value--
+		}
+	}
+
+	function setCount(value) {
+		if (value > -1) {
+			count.value = value
+		}
+	}
+
+	function resetCount() {
+		count.value = 0
+	}
+
+	return { count, doubleCount, increment, decrement, setCount, resetCount }
+})
